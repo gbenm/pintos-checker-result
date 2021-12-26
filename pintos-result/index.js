f25dc0f3fc0bbe8a677fda0042d429ba9c9ff6d1
gbenm <benyamin.galeano@gmail.com>
2021-12-25 18:38:18 -0600

    Sin comprimir

--- pintos-result/index.js
+++ pintos-result/index.js
@@ -1,6 +1,15 @@
-const core = require("@actions/core");
+const { getInput, setFailed } = require("@actions/core");
+//const { execSync } = require("child_process");
 
-const path = "./pintos.checker.js";
+//const path = `${__dirname}/../pintos.checker.mjs`;
+const path = `/home/runner/work/pintos-checker-result-test/pintos-checker-result-test/pintos.checker.mjs`;
+//console.log(__dirname);
+//console.log(execSync(`ls --color /home/runner/work/pintos-checker-result-test/pintos-checker-result-test/`).toString());
+//console.log("------------------");
+//console.log(execSync(`ls --color /home/runner/work/pintos-checker-result-test/`).toString());
+
+
+console.log(path);
 
 const getGrade = (result, {gradeExpr}) => {
   let {grade, total} = gradeExpr.exec(result).groups;
@@ -13,50 +22,16 @@ const getGrade = (result, {gradeExpr}) => {
 const executeCheck = (name, utils, {grade, total}) => utils[name](grade, total);
 
 import(path).then((utils) => {
-  const results = core.getInput("results");
+  const results = getInput("results");
   const grade = getGrade(results, utils);
   const pass = executeCheck("threads", utils, grade);
 
   if (!pass) {
-    core.setFailed("No cumple la regla");
+    setFailed("No cumple la regla");
   }
 
   const {showResults} = utils;
 
   showResults(console.log, results, grade.grade, grade.total);
 });
-
-//let checkerModule = flk.task((resolver) => {
-  //import(path).then(resolver.resolve).catch(resolver.reject);
-//});
-
-//// getGrade :: String -> {gradeExpr: RegExpr} -> {grade: Float, total: Float}
-//const getGrade = curry((result, {gradeExpr}) => {
-  //let {grade, total} = gradeExpr.exec(result).groups;
-  //grade = parseFloat(grade);
-  //total = parseFloat(total);
-
-  //return {grade, total};
-//});
-
-//const getGradeWithRegExp = getGrade(core.getInput("results"));
-
-//// Task of {grade: Float, total: Float}
-//const grade = map(getGradeWithRegExp, checkerModule);
-
-//const buildCheck = curry((name, checker) => {
-  //return map(prop(name), checker);
-//});
-
-//const checker = buildCheck("threads", checkerModule);
-
-//const app = checker.chain((executeCheck) => map(executeCheck, grade));
-
-//app.run().promise().then((pass) => {
-  //if (!pass) {
-    //core.setFailed("No cumple la regla");
-  //}
-//});
-
-
 
