22d8d22d003a628f0acdfc55290f5d6cc0143c32
gbenm <benyamin.galeano@gmail.com>
2021-12-25 19:28:18 -0600

    Todas la reglas, agregando verificación que tenga resultados

--- pintos-result/dist/index.js
+++ pintos-result/dist/index.js
@@ -1677,18 +1677,17 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const { getInput, setFailed } = __nccwpck_require__(626);
-//const { execSync } = require("child_process");
 
-//const path = `${__dirname}/../pintos.checker.mjs`;
 const path = `../../pintos.checker.mjs`;
-//console.log(__dirname);
-//console.log(execSync(`ls --color /home/runner/work/pintos-checker-result-test/pintos-checker-result-test/`).toString());
-//console.log("------------------");
-//console.log(execSync(`ls --color /home/runner/work/pintos-checker-result-test/`).toString());
-
-console.log(path);
+const phase = getInput("phase");
+const results = getInput("results");
 
 const getGrade = (result, {gradeExpr}) => {
+  if (!result) {
+    setFailed("La etapa anterior no produjo resultados");
+    process.exit(1);
+  }
+
   let {grade, total} = gradeExpr.exec(result).groups;
   grade = parseFloat(grade);
   total = parseFloat(total);
@@ -1699,9 +1698,8 @@ const getGrade = (result, {gradeExpr}) => {
 const executeCheck = (name, utils, {grade, total}) => utils[name](grade, total);
 
 import(path).then((utils) => {
-  const results = getInput("results");
   const grade = getGrade(results, utils);
-  const pass = executeCheck("threads", utils, grade);
+  const pass = executeCheck(phase, utils, grade);
 
   if (!pass) {
     setFailed("No cumple la regla");
--- pintos-result/index.js
+++ pintos-result/index.js
@@ -1,16 +1,15 @@
 const { getInput, setFailed } = require("@actions/core");
-//const { execSync } = require("child_process");
 
-//const path = `${__dirname}/../pintos.checker.mjs`;
 const path = `../../pintos.checker.mjs`;
-//console.log(__dirname);
-//console.log(execSync(`ls --color /home/runner/work/pintos-checker-result-test/pintos-checker-result-test/`).toString());
-//console.log("------------------");
-//console.log(execSync(`ls --color /home/runner/work/pintos-checker-result-test/`).toString());
-
-console.log(path);
+const phase = getInput("phase");
+const results = getInput("results");
 
 const getGrade = (result, {gradeExpr}) => {
+  if (!result) {
+    setFailed("La etapa anterior no produjo resultados");
+    process.exit(1);
+  }
+
   let {grade, total} = gradeExpr.exec(result).groups;
   grade = parseFloat(grade);
   total = parseFloat(total);
@@ -21,9 +20,8 @@ const getGrade = (result, {gradeExpr}) => {
 const executeCheck = (name, utils, {grade, total}) => utils[name](grade, total);
 
 customImport(path).then((utils) => {
-  const results = getInput("results");
   const grade = getGrade(results, utils);
-  const pass = executeCheck("threads", utils, grade);
+  const pass = executeCheck(phase, utils, grade);
 
   if (!pass) {
     setFailed("No cumple la regla");
